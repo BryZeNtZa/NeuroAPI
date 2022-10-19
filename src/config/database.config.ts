be39c3218c75ef9f9f32160bf350afb1991106e0
Bryze Ntza <ntbryze@gmail.com>
2022-10-19 20:35:56 +0100

    adressing mongose unable to connect

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  uri: `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+  uri: `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?directConnection=true`,
   useCreateIndex: true,
 }));
